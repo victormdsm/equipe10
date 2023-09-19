87d4f53fa1352d68a9d8025128fb919c000b8b25
victormdsm <victor.de.souza.muller@gmail.com>
2023-09-19 17:40:44 -0300

    Sidebar

---
+++ login.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <link rel="shortcut icon" href="assets/mamonha big.png" type="image/x-icon" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="views/css/style.css" rel="stylesheet" />
+  <title>Login page</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" 
+  integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
+</head>
+
+
+<body>
+  <main>
+  
+    <section class="d-flex justify-content-center align-items-center fundo ">
+      
+      <div >
+        <img src="assets/mamonha big.png"  alt="mamonha logo">
+      </div>
+
+      <div class="d-flex" style="height: 300px;">
+        <div class="vr"></div>
+      </div>
+      
+      <div class="borda   ms-5 cor ">
+        
+        <form >
+          <div class="m-3">
+            <label for="exampleInputEmail1" class="form-label">Email</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+          </div>
+          <div class="m-3">
+            <label for="exampleInputPassword1" class="form-label">Senha</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
+            <button type="button" class="btn p-0" data-bs-toggle="modal" data-bs-target="#modalrecsenha">
+              Esqueceu sua senha ?
+            </button>
+          </div>
+          <div class="m-3 form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">Lembre minha senha</label>
+          </div>
+          <div class="container">
+            <button type="submit" class="btn bt-cor w-100 p-2 mt-3 py-2 mb-3">Login</button>
+          </div>
+        </form>
+      </div> 
+    </section>
+  </main> 
+
+
+  <?php include 'footer.html' ?>
+  
+  
+
+  <!-- Modal Recuperar Senha -->
+  <div class="modal fade " id="modalrecsenha" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h2 class="modal-title fs-5" id="exampleModalLabel">Recuperar Senha</h2>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <form class="container ">
+            <div class="d-flex flex-column ">
+              <label for="inputEmail4" class="form-label fw-bold">Email:</label>
+              <input type="email" class="form-control" id="inputEmail4" placeholder="email@example.com">
+            </div>
+          </form>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn bt-cor" data-bs-dismiss="modal">Recuperar Senha</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" 
+  integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
